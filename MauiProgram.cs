329c44dda35a44c470f1718956460dc79d2ae14e
rkoort <ristokoort@yahoo.com>
2023-05-30 15:21:55 +0300

    Appshell, mauiprogram,schedulepage changes

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -12,7 +12,8 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
-        builder.Services.AddTransient<SchedulePage>();
+        builder.Services.AddSingleton<ScheduleDay1Page>();
+        builder.Services.AddSingleton<ScheduleDay2Page>();
         builder.Services.AddTransient<ScheduleViewModel>();
 
         return builder.Build();
