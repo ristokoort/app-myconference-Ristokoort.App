20c9631da4a405a336c5572136e321c04aadc3a8
rkoort <ristokoort@yahoo.com>
2023-05-30 15:24:18 +0300

    Schedulepage changes, 1 error

--- Pages/SchedulePage.xaml.cs
+++ Pages/SchedulePage.xaml.cs
@@ -1,17 +1,43 @@
 namespace app_myconference.Pages;
 
-public partial class SchedulePage : ContentPage
+public partial class ScheduleDay1Page : SchedulePage
 {
-    readonly ScheduleViewModel vm;
-    public static int Day { get; set; }
-    public SchedulePage(ScheduleViewModel vm)
+    public ScheduleDay1Page(ScheduleViewModel vm) : base()
     {
-        InitializeComponent();
-
-        
-        Day++;
-        vm.Day = Day;
-
-        BindingContext = this.vm = vm;
+        vm.Day = 1;
+        Title = "Schedule - Day 1";
+        BindingContext = vm;
     }
 }
+
+public partial class ScheduleDay2Page : SchedulePage
+{
+    public ScheduleDay2Page(ScheduleViewModel vm) : base()
+    {
+        vm.Day = 2;
+        Title = "Schedule - Day 2";
+        BindingContext = vm;
+    }
+}
+public partial class SchedulePage : ContentPage
+{
+    ScheduleViewModel vm;
+    ScheduleViewModel VM => vm ??= BindingContext as ScheduleViewModel;
+    public SchedulePage()
+    {
+        InitializeComponent();
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        if (VM.Schedule.Count == 0)
+            await VM.LoadDataCommand.ExecuteAsync(null);
+    }
+    protected override void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        base.OnNavigatedTo(args);
+    }
+}
+
+
+
