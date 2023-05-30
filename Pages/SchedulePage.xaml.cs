297bb6fc37060a78d5803da882f3c5684e6b2bba
rkoort <ristokoort@yahoo.com>
2023-05-30 15:18:00 +0300

    Change schedulepages

--- Pages/SchedulePage.xaml.cs
+++ Pages/SchedulePage.xaml.cs
@@ -3,26 +3,15 @@ namespace app_myconference.Pages;
 public partial class SchedulePage : ContentPage
 {
     readonly ScheduleViewModel vm;
+    public static int Day { get; set; }
     public SchedulePage(ScheduleViewModel vm)
     {
         InitializeComponent();
 
-        switch (Title)
-        {
-            case "Day 1":
-                vm.Day = 1;
-                break;
-            case "Day 2":
-                vm.Day = 2;
-                break;
-        }
+        
+        Day++;
+        vm.Day = Day;
 
         BindingContext = this.vm = vm;
-    }
-    protected override async void OnNavigatedTo(NavigatedToEventArgs args)
-    {
-        base.OnNavigatedTo(args);
-        if (vm.Schedule.Count == 0)
-            await vm.LoadDataCommand.ExecuteAsync(null);
     }
 }
